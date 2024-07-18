53c262c1837df4934132ed8f298b7bb5d44ccb83
Umarchik357 <bsmagstrange111@gmail.com>
2024-07-18 09:48:08 +0500

    install aiogram

---
+++ config.py
@@ -0,0 +1,17 @@
+from aiogram import Bot,Dispatcher,types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+API_TOKEN = env('API_TOKEN')
+ADMIN_ID = env('ADMIN_ID')
+CHANNEL_ID = env('CHANNEL_ID')
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
